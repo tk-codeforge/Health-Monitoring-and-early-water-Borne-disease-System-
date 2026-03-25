72576d78b68d86fd3600f5aa088ca0d84ab60d32
tk-codeforge <tanmay.kumar54512@gmail.com>
2026-03-25 11:29:59 +0530

    Delete models directory

--- models/symptom.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const symptomSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  symptoms: { type: [String], required: true }, // e.g., ["diarrhea", "nausea"]
-  date: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Symptom', symptomSchema);
